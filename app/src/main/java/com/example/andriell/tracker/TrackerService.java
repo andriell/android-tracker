21dabfc1c15b4a3bf71f5fecde816440f09f4023
andriell <root.andriell@gmail.com>
2017-12-20 12:23:37 +0300

    start

--- app/src/main/java/com/example/andriell/tracker/TrackerService.java
+++ app/src/main/java/com/example/andriell/tracker/TrackerService.java
@@ -10,11 +10,9 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Binder;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class TrackerService extends Service {
@@ -25,7 +23,6 @@ public class TrackerService extends Service {
     private Location lastLocation = null;
     private double distance;
     private SizedStack<String> stack = new SizedStack(10);
-    private final Handler handler = new Handler();
 
     public TrackerService() {
         super();
@@ -40,7 +37,6 @@ public class TrackerService extends Service {
     @Override
     public void onCreate() {
         stack.push("onCreate");
-        delay();
         LocationManager locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, locationListener);
     }
@@ -83,15 +79,6 @@ public class TrackerService extends Service {
         startForeground(DEFAULT_NOTIFICATION_ID, notification);
     }
 
-    private void delay() {
-        handler.post(new Runnable() {
-            public void run() {
-                stack.push(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
-                handler.postDelayed(this, 1000);
-            }
-        });
-    }
-
     public double getDistance() {
         return distance;
     }
@@ -113,6 +100,8 @@ public class TrackerService extends Service {
     public class TrackerLocationListener implements LocationListener {
         @Override
         public void onLocationChanged(Location location) {
+            String s = String.format("%1$,.8f %2$,.8f %3$tT", location.getLongitude(), location.getLatitude(), new Date());
+            stack.push(s);
             if (lastLocation == null) {
                 lastLocation = location;
             }
