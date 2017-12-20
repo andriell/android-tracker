8306806aa2327c58c3c9474c27592fba90e4af4d
andriell <root.andriell@gmail.com>
2017-12-20 12:10:52 +0300

    start

--- app/src/main/java/com/example/andriell/tracker/TrackerService.java
+++ app/src/main/java/com/example/andriell/tracker/TrackerService.java
@@ -1,5 +1,7 @@
 package com.example.andriell.tracker;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -10,12 +12,14 @@ import android.os.Binder;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
-import android.util.Log;
+import android.support.v4.app.NotificationCompat;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class TrackerService extends Service {
+    private static final int DEFAULT_NOTIFICATION_ID = 101;
+
     private final IBinder binder = new TrackerBinder();
     private final LocationListener locationListener = new TrackerLocationListener();
     private Location lastLocation = null;
@@ -44,11 +48,42 @@ public class TrackerService extends Service {
     @Override
     public void onDestroy() {
         stack.push("onDestroy");
-        //LocationManager locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        //locManager.removeUpdates(locationListener);
     }
 
-    public void delay() {
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        sendNotification("Tracker service started", "Tracker", "Started");
+        return Service.START_STICKY;
+    }
+
+    //Send custom notification
+    private void sendNotification(String Ticker,String Title,String Text) {
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        notificationIntent.setAction(Intent.ACTION_MAIN);
+        notificationIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(getApplicationContext(), 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setContentIntent(contentIntent)
+                .setOngoing(true)   //Can't be swiped out
+                .setSmallIcon(R.mipmap.ic_launcher)
+                //.setLargeIcon(BitmapFactory.decodeResource(res, R.drawable.large))   // большая картинка
+                .setTicker(Ticker)
+                .setContentTitle(Title) //Заголовок
+                .setContentText(Text) // Текст уведомления
+                .setWhen(System.currentTimeMillis());
+
+        Notification notification;
+        if (android.os.Build.VERSION.SDK_INT<=15) {
+            notification = builder.getNotification(); // API-15 and lower
+        }else{
+            notification = builder.build();
+        }
+
+        startForeground(DEFAULT_NOTIFICATION_ID, notification);
+    }
+
+    private void delay() {
         handler.post(new Runnable() {
             public void run() {
                 stack.push(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
